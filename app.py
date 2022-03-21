437c7874ad73d3e6db224241cb9040f2fd0edf09
Avi Chaudhary <74415566+avichaudhary0001@users.noreply.github.com>
2022-03-21 13:06:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,13 +41,9 @@ input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
 
-    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
